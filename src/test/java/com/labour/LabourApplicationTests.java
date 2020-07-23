e130792e51d54dda047adfa9b81695444f290df0
a531227909 <531227909@qq.com>
2020-07-24 02:19:14 +0800

    修改代码结构

--- src/test/java/com/labour/LabourApplicationTests.java
+++ src/test/java/com/labour/LabourApplicationTests.java
@@ -1,14 +1,14 @@
-package com.seasefun.lottery;
+package com.labour;
 
-import com.seasefun.lottery.init.LotteryApplication;
+import com.labour.init.LabourApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = LotteryApplication.class)
-public class LotteryApplicationTests {
+@SpringBootTest(classes = LabourApplication.class)
+public class LabourApplicationTests {
 
     @Test
     public void contextLoads() {
